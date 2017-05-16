07a32aa21a7f0dbe67200748b91ad25b672a131f
Cody Winchester <cjwinchester@gmail.com>
2017-05-16 17:04:06 -0500

    l2 + twitter etc.

---
+++ 2017/python-in-the-wild.md
@@ -0,0 +1,15 @@
+# Python in the wild
+
+* [Chicago Tribune campaign finance Twitter bot](https://twitter.com/ilcampaigncash) - [see it on GitHub](https://github.com/newsapps/ilcampaignfinance)
+
+* [Buzzfeed: NICS firearms background checks](https://github.com/BuzzFeedNews/nics-firearm-background-checks)
+
+* [Buzzfeed: H-2 Visa/Enforcement Data and Analysis](https://github.com/BuzzFeedNews/2015-07-h2-visas-and-enforcement)
+
+* [Omaha World-Herald campaign finance parser](https://github.com/owh-projects/nadc_data) - [see the public-facing app, too](http://dataomaha.com/campaign-finance)
+
+* [NYT: clerk](https://github.com/newsdev/nyt-clerk)
+
+* [LAT: python-documentcloud](https://python-documentcloud.readthedocs.io/en/latest/)
+
+* [NYT: pyfec](https://github.com/newsdev/nyt-pyfec)
--- 2017/tweet.py
+++ 2017/tweet.py
@@ -1,6 +1,9 @@
-import tweepy
 import os
+import csv
+import statistics
+import tweepy
 
+# set up twitter client
 consumer_key = os.environ.get('djnf_tw_consumer_key', None)
 consumer_secret = os.environ.get('djnf_tw_consumer_secret', None)
 access_token = os.environ.get('djnf_tw_token', None)
@@ -11,4 +14,17 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-api.update_status(status='hello world')
+# spin thru MLB data, find descriptive statistics
+with open('data/mlb.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    salaries = [float(row['SALARY']) for row in reader]
+    count = len(salaries)
+    total = "${:,}".format(int(sum(salaries)))
+    average = "${:,}".format(int(statistics.mean(salaries)))
+    median = "${:,}".format(int(statistics.median(salaries)))
+
+    thing_to_tweet = 'MLB opening day 2016 stats:\n- Players: {}\n' \
+                     '- Total payroll: {}\n- Average: {}\n' \
+                     '- Median: {}'.format(count, total, average, median)
+
+    api.update_status(status=thing_to_tweet)
